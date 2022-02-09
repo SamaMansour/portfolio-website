4ce7d23b5e7c4b6adf2b2145485d617fdca78bef
Sama Mansour <samamansour306@gmail.com>
2022-02-09 22:52:53 +0200

    first

---
+++ src/components/intro/intro.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './intro.css';
+import me from '../../img/me.png'
+
+const Intro = () => {
+  return (
+    <div className = "i">
+        <div className = "i-left">
+            <div className = "i-left-wrapper">
+                <h2 className = "i-intro"> Hello, My name is</h2>
+                <h2 className = "i-name"> Sama Mansour </h2>
+                <div className = "i-title"> 
+                <div className = "i-title-wrapper">
+                    <div className = "i-title-item">Fresh Graduate</div>
+                   
+                </div>
+
+                
+                </div>
+                
+                 <div > Looking for an entry level job as a full stack developer</div>
+            </div>
+        </div>
+        <div className = "i-right">
+            <div className = "i-bg"></div>
+            <img src= {me} alt="me" className="i-img"/>
+        </div>
+    </div>
+  )
+}
+
+export default Intro     
---
+++ src/components/productList/productList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './productList.css'
+import Product from '../product/product'
+
+const productList = () => {
+  return (
+ <div className="p1">
+     <div className="p1-texts">
+         <h2  className='title'>Projects List</h2>
+         <div className="underline"></div>
+         <p className="p1-desc">
+             Creating multiple practicing projects using different languages <br/> 
+             MERN stack , django, ASP.NET , AngularJS and <br/>
+             Flutter as a cross-platform ...
+
+         </p>
+     </div>
+     <div className="p1-list">
+        
+
+     </div>
+ </div>
+  )
+}
+
+export default productList
